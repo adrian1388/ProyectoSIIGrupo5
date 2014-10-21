afa2c30b7a43c477f921387a1fe8464875e53d06
Marvincin <arthas_2301_lich@live.com>
2014-10-21 16:29:41 -0500

    Suma
    Se añadio la suma

--- HelloWorld/HelloWorld/Source.cpp
+++ HelloWorld/HelloWorld/Source.cpp
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
 int main(){
+	int a=0,b=0;
 	printf("Hello World");
+	printf("Ingrese 2 numeros\n");
+	printf("Ingrese el primer número\n");
+	scanf("%d",&a);
+	printf("Ingrese el primer número\n");
+	scanf("%d",&b);
+	printf("La suma de los numeros es %d",a+b);
 	return 0;
 }
