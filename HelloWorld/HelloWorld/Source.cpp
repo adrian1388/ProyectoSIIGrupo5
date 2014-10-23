960e31893937225e41bdb048da3b6127c0478e45
razerjon24 <razerjon24@gmail.com>
2014-10-23 08:59:04 -0500

    Branch division
    Se crea el branch con respecto a la division.

--- HelloWorld/HelloWorld/Source.cpp
+++ HelloWorld/HelloWorld/Source.cpp
@@ -9,5 +9,19 @@ int main(){
 	printf("Ingrese el primer número\n");
 	scanf("%d",&b);
 	printf("La suma de los numeros es %d",a+b);
+	//Parte de la division
+	printf("Division \n");
+	printf("Ingrese 2 numeros\n");
+	printf("Ingrese el dividendo\n");
+	scanf("%d", &a);
+	printf("Ingrese el divisor\n");
+	scanf("%d", &b);
+	while (b == 0) {
+		printf("Error: El divisor no puede ser cero");
+		printf("Ingrese el divisor\n");
+		scanf("%d", &b);
+	} 
+	printf("La division de los numeros es %d", a / b);
 	return 0;
+	
 }
