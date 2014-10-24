2516f08eb50cad16bf9f4c7f8be0c5ebffbf0d59
JordyVasquez <jgvasque@espol.edu.ec>
2014-10-23 23:16:15 -0500

    Potencia

--- HelloWorld/HelloWorld/Source.cpp
+++ HelloWorld/HelloWorld/Source.cpp
@@ -1,31 +1,81 @@
 #include <stdio.h>
 
+
+int potencia(int a, int b);
+
 int main(){
+
 	int a=0,b=0;
+
 	printf("Hello World");
+
 	printf("Ingrese 2 numeros\n");
+
 	printf("Ingrese el primer número\n");
+
 	scanf("%d",&a);
 	printf("Ingrese el primer número\n");
+
 	scanf("%d",&b);
 	printf("La suma de los numeros es %d",a+b);
-<<<<<<< HEAD
+
+
 	//Parte de la division
-	printf("Division \n");
+
+	printf("\nDivision \n");
+
 	printf("Ingrese 2 numeros\n");
+
 	printf("Ingrese el dividendo\n");
+
 	scanf("%d", &a);
+
 	printf("Ingrese el divisor\n");
+
 	scanf("%d", &b);
+
 	while (b == 0) {
+
 		printf("Error: El divisor no puede ser cero");
+
 		printf("Ingrese el divisor\n");
+
 		scanf("%d", &b);
-	} 
-	printf("La division de los numeros es %d", a / b);
-=======
-	printf("/n/nLa multiplicacion de los numeros es %d",a*b);
->>>>>>> 06f20e2dd1c5088edca4a0ab678aa5d8b75602c9
+
+	}
+ 
+	printf("La division de los numeros es %d", a/b);
+
+
+	printf("\n La multiplicacion de los numeros es: %d",a*b);
+
+	printf("\nIngrese el primer número para la potencia\n");
+
+	scanf("%d",&a);
+	printf("Ingrese la potencia\n");
+
+	scanf("%d",&b);
+	int re=potencia(a,b);
+
+	printf("La Potencia es : %d", re);
+
 	return 0;
+
 	
+}
+
+int potencia(int a,int b){
+
+int i=0,resultado=a;
+
+
+	for(i=0;i<b-1;i++){
+
+	resultado=resultado*a;
+
+	}
+
+return resultado;
+
+
 }
